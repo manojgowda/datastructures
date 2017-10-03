88e00e753b94d2f58f02ef2ca750d5a3b06a07fb
Manoj H N <manojgowdanssm@gmail.com>
2017-10-03 17:06:50 +0530

    datastructures: initial commit.

---
+++ datastructures/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='datastructures',
+      version='0.1',
+      description='All Kind of Data structures with algorithms.',
+      url='https://github.com/manojgowda/datastructures',
+      author='Manoj H N',
+      author_email='manojgowdanssm@gmail.com',
+      license='NOT YET',
+      packages=['datastructures'],
+      zip_safe=False)
